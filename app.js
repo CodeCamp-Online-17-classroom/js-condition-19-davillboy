db5ce07fa3e0ffc62bd38995c2ba3ab95466512a
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 11:21:04 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+ <script>
+        let score = prompt('ใส่คะแนนของคุณ');
+        let grade =
+        (score >= 80) ? 'A' :
+        (score >= 70) ? 'B' :
+        (score >= 60) ? 'C' :
+        (score >= 50) ? 'D' :
+        'F';
+
+        alert(`เกรดที่ได้: ${grade}`);
+
+  </script>
